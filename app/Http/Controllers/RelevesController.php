99fc1f5748b8c2d296447410b144edee2d1f96e7
epiphanie8 <epiphanieatta8@gmail.com>
2023-09-25 17:58:23 +0000

    api releves de note: rafficher les differents semestre de chaque annee academique

--- app/Http/Controllers/RelevesController.php
+++ app/Http/Controllers/RelevesController.php
@@ -6,15 +6,76 @@ use App\Models\Etudiant;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
+class RelevesController extends Controller {
+    // public function getannee( $Matri_Elev ) {
+    //     $etudiants = Etudiant::where( 'Matri_Elev', $Matri_Elev )->first();
 
-class RelevesController extends Controller
-{
-    public function getannee($Matri_Elev)
-    {
-        $etudiants = Etudiant::where('Matri_Elev', $Matri_Elev)->first();
+    //     if ( !$etudiants ) {
+    //         return Response()->json( [
+    //             'success' => false,
+    //             'status' => 404,
+    //             'message' => 'L\'élève ayant le matricule ' . $Matri_Elev . ' est introvable.',
+    //         ]);
+    //     }
+    //     return $this->getAnneeDeliberer($Matri_Elev);
+    // }
 
-        if (!$etudiants) {
-            return Response()->json([
+
+    // protected function getAnneeDeliberer($id)
+    // {
+    //     $etudiant = DB::table('DELIBERER as delib')
+    //         ->selectRaw('DISTINCT( delib.Annee ) as Annee')
+    //         ->join('etudiants as etud', 'delib.Matri_Elev', '=', 'etud.Matri_Elev' )
+    //             ->join( 'UE', 'delib.CodeUE', '=', 'UE.CodeUE' )
+    //             ->join( 'GROUPE as group', 'delib.IdGroupe', '=', 'group.IdGroupe' )
+    //             ->join( 'Examen as exam', 'group.Code_Examen', '=', 'exam.Code_Examen' )
+    //             ->join( 'ECUE', 'UE.CodeUE', '=', 'ECUE.CodeUE' )
+    //             ->where( 'delib.Matri_Elev', $id )
+    //             ->pluck( 'Annee' );
+
+    //             if ( $etudiant->isEmpty() ) {
+    //                 return Response()->json( [
+    //                     'success' => false,
+    //                     'status' => 404,
+    //                     'message' => 'Aucun bulletin disponible pour cet etudiant',
+    //                 ] );
+    //             }
+    //             $classeParAnnee = [];
+
+    //             foreach ( $etudiant as $annee ) {
+    //                 $annee = trim( $annee );
+    //                 $classeParAnnee[ $annee ] = $this->getclasse( $id, $annee );
+    //             }
+    //             if ( !empty( $classeParAnnee ) ) {
+    //                 return Response()->json( [
+    //                     'success' => true,
+    //                     'status' => 200,
+    //                     'classes' => $classeParAnnee,
+    //                 ] );
+    //             } else {
+    //                 return Response()->json( [
+    //                     'success' => false,
+    //                     'status' => 404,
+    //                     'message' => 'Aucun bulletin disponible pour cet étudiant',
+    //                 ] );
+    //             }
+    //         }
+    //         protected function getclasse( $matricule, $etudiantannee ) {
+    //             $etud = DB::table( 'Historique as histo' )
+    //             ->selectRaw( 'cla.Code_Cla, histo.AnneeSco_Elev as annee' )
+    //             ->join( 'Détail Classes as detcla', 'detcla.Code_Detcla', '=', 'histo.Code_Detcla' )
+    //             ->join( 'Classes Info Générales as cla', 'detcla.Code_Cla', '=', 'cla.Code_Cla' )
+    //             ->where( 'histo.Matri_Elev', $matricule )
+    //             ->where( 'histo.AnneeSco_Elev', $etudiantannee )
+    //             ->get();
+    //             return $etud;
+    //         }
+
+        public function getannee( $Matri_Elev ) {
+        $etudiants = Etudiant::where( 'Matri_Elev', $Matri_Elev )->first();
+
+        if ( !$etudiants ) {
+            return Response()->json( [
                 'success' => false,
                 'status' => 404,
                 'message' => 'L\'élève ayant le matricule ' . $Matri_Elev . ' est introvable.',
@@ -24,54 +85,130 @@ class RelevesController extends Controller
     }
 
 
-    protected function getAnneeDeliberer($id)
+        protected function getAnneeDeliberer($id)
     {
-        $etudiant = DB::table('DELIBERER as delib')
-            ->selectRaw('DISTINCT(delib.Annee) as Annee')
-            ->join('etudiants as etud', 'delib.Matri_Elev', '=', 'etud.Matri_Elev')
-            ->join('UE', 'delib.CodeUE', '=', 'UE.CodeUE')
-            ->join('GROUPE as group', 'delib.IdGroupe', '=', 'group.IdGroupe')
-            ->join('Examen as exam', 'group.Code_Examen', '=', 'exam.Code_Examen')
-            ->join('ECUE', 'UE.CodeUE', '=', 'ECUE.CodeUE')
-            ->where('delib.Matri_Elev', $id)
-            ->pluck('Annee');
-
-        if ($etudiant->isEmpty()) {
-            return Response()->json([
-                'success' => false,
-                'status' => 404,
-                'message' => 'Aucun bulletin disponible pour cet etudiant',
-            ]);
+        $annees_academiques = DB::table('DELIBERER as delib')
+        ->select('delib.Annee')
+        ->where('delib.Matri_Elev', $id)
+        ->distinct()
+        ->get();
+    
+        $annees_formattees = [];
+        
+        foreach ($annees_academiques as $annee) {
+            $annee_formattee = trim(str_replace('/', '-', $annee->Annee));
+            $annees_formattees[] = $annee_formattee;
         }
-        $classeParAnnee = [];
-
-        foreach ($etudiant as $annee) {
-            $annee = trim($annee);
-            $classeParAnnee[$annee] = $this->getclasse($id, $annee);
-        }
-        if (!empty($classeParAnnee)) {
-            return Response()->json([
-                'success' => true,
-                'status' => 200,
-                'classes' => $classeParAnnee,
-            ]);
-        } else {
-            return Response()->json([
-                'success' => false,
-                'status' => 404,
-                'message' => 'Aucun bulletin disponible pour cet étudiant',
-            ]);
-        }
+                if ($annees_academiques->isEmpty() ) {
+                    return Response()->json( [
+                        'success' => false,
+                        'status' => 404,
+                        'message' => 'Aucun bulletin disponible pour cet etudiant',
+                    ] );
+                }
+                else{
+                    return Response()->json( [
+                        'success' => true,
+                        'status' => 200,
+                        'Annee_Academique' => $annees_formattees,
+                   ] );
+                }
     }
-    protected function getclasse($matricule, $etudiantannee)
-    {
-        $etud = DB::table('Historique as histo')
-            ->selectRaw('cla.Code_Cla, histo.AnneeSco_Elev as annee')
-            ->join('Détail Classes as detcla', 'detcla.Code_Detcla', '=', 'histo.Code_Detcla')
-            ->join('Classes Info Générales as cla', 'detcla.Code_Cla', '=', 'cla.Code_Cla')
-            ->where('histo.Matri_Elev', $matricule)
-            ->where('histo.AnneeSco_Elev', $etudiantannee)
-            ->get();
-        return $etud;
+     public function getreleve($Matri_Elev,$annee_acad)
+     {
+        //retourner le semestre
+        $semestre  = DB::table('DELIBERER as delib')
+            ->selectRaw('sem.Code_semestre as semestre')
+            ->join('etudiants as etud', 'delib.Matri_Elev', '=', 'etud.Matri_Elev' )
+            ->join( 'UE', 'delib.CodeUE', '=', 'UE.CodeUE' )
+            ->join( 'GROUPE as group', 'delib.IdGroupe', '=', 'group.IdGroupe' )
+            ->join( 'Examen as exam', 'group.Code_Examen', '=', 'exam.Code_Examen' )
+            ->join( 'ECUE', 'UE.CodeUE', '=', 'ECUE.CodeUE' )
+            ->join( 'SEMESTRE as sem', 'exam.Code_semestre', '=', 'sem.Code_semestre' )
+            ->where( 'delib.Matri_Elev', $Matri_Elev )
+            ->where( 'delib.Annee', trim(str_replace('-', '/', $annee_acad)))
+            ->distinct()
+            ->pluck('semestre');
+            
+            //retourner la session
+
+            $sessions = DB::table('DELIBERER as delib')
+            ->select('sess.CODE_SESS as session')
+            ->join('etudiants as etud', 'delib.Matri_Elev', '=', 'etud.Matri_Elev' )
+            ->join( 'UE', 'delib.CodeUE', '=', 'UE.CodeUE' )
+            ->join( 'GROUPE as group', 'delib.IdGroupe', '=', 'group.IdGroupe' )
+            ->join( 'Examen as exam', 'group.Code_Examen', '=', 'exam.Code_Examen' )
+            ->join( 'ECUE', 'UE.CodeUE', '=', 'ECUE.CodeUE' )
+            ->join( 'session as sess', 'exam.COD_SESS', '=', 'sess.CODE_SESS' )
+            ->where( 'sess.CODE_SESS', 'Sess2')
+            ->where( 'delib.Matri_Elev', $Matri_Elev )
+            ->where( 'delib.Annee', trim(str_replace('-', '/', $annee_acad) ))
+            ->distinct()
+            ->pluck('session');
+
+
+            if($semestre->isEmpty())
+            {
+                return Response()->json( [
+                    'success' => false,
+                    'status' => 404,
+                    'message' => 'Aucun bulletin disponible pour cet etudiant',
+                    ] );
+            }
+            else{
+                if($sessions->isEmpty())
+                {
+                return Response()->json( [
+                    'success' => true,
+                    'status' => 200,
+                    'Bulletin' => $semestre
+                    ] );
+                }
+                else{
+                    return Response()->json( [
+                        'success' => true,
+                        'status' => 200,
+                        'Bulletin' => $semestre,
+                        'session2' => $sessions
+                        ] );
+    
+                }
+
+            }
+
+       // $sessions = [];
+
+        // foreach ($semestre as $code_semestre) {
+        //     $sessionsSemestre = DB::table('DELIBERER as delib')
+        //         ->select('sess.CODE_SESS as session')
+        //         ->join('etudiants as etud', 'delib.Matri_Elev', '=', 'etud.Matri_Elev' )
+        //         ->join( 'UE', 'delib.CodeUE', '=', 'UE.CodeUE' )
+        //         ->join( 'GROUPE as group', 'delib.IdGroupe', '=', 'group.IdGroupe' )
+        //         ->join( 'Examen as exam', 'group.Code_Examen', '=', 'exam.Code_Examen' )
+        //         ->join( 'ECUE', 'UE.CodeUE', '=', 'ECUE.CodeUE' )
+        //         ->join( 'SEMESTRE as sem', 'exam.Code_semestre', '=', 'sem.Code_semestre' )
+        //         ->join( 'session as sess', 'exam.COD_SESS', '=', 'sess.CODE_SESS' )
+        //         ->where( 'delib.Matri_Elev', $Matri_Elev )
+        //         ->where( 'delib.Annee', trim(str_replace('-', '/', $annee_acad) ))
+        //         ->distinct()
+        //         ->pluck('session');
+            
+        //     $sessions[$code_semestre] = $sessionsSemestre;
+        // }
+        // if ( !empty( $sessions ) ) {
+        //         return Response()->json( [
+        //             'success' => true,
+        //             'status' => 200,
+        //             'Bulletin' => $sessions
+        //             ] );
+        // } else {
+        //             return Response()->json( [
+        //                 'success' => false,
+        //                 'status' => 404,
+        //                 'message' => 'Aucun bulletin disponible pour cet étudiant',
+        //             ] );
+        //         }
+        //     }
+
+     }
     }
-}
--- routes/api.php
+++ routes/api.php
@@ -13,40 +13,40 @@ use App\Http\Controllers\UserController;
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| be assigned to the 'api' middleware group. Make something great!
 |
 */
 
-Route::post('forgetpassword', 'App\Http\Controllers\AuthController@forgetPassword');
+Route::post( 'forgetpassword', 'App\Http\Controllers\AuthController@forgetPassword' );
 
 Route::group(
     [
         'middleware' => 'api',
 
     ],
-    function ($router) {
-        // AUTHENTICATION
-        Route::put('register/{Matri_Elev}', 'App\Http\Controllers\AuthController@register');
-        Route::post('login', 'App\Http\Controllers\AuthController@login');
-        Route::get('refreshtoken', 'App\Http\Controllers\AuthController@refreshToken');
-        Route::get('profile', 'App\Http\Controllers\AuthController@profile');
-        Route::post('logout', 'App\Http\Controllers\AuthController@logout');
 
+    function ( $router ) {
+        // AUTHENTICATION
+        Route::put( 'register/{Matri_Elev}', 'App\Http\Controllers\AuthController@register' );
+        Route::post( 'login', 'App\Http\Controllers\AuthController@login' );
+        Route::get( 'refreshtoken', 'App\Http\Controllers\AuthController@refreshToken' );
+        Route::get( 'profile', 'App\Http\Controllers\AuthController@profile' );
+        Route::post( 'logout', 'App\Http\Controllers\AuthController@logout' );
 
         // API USER
-        Route::put('update_user/{Matri_Elev}', 'App\Http\Controllers\UserController@update_user');
-
+        Route::put( 'update_user/{Matri_Elev}', 'App\Http\Controllers\UserController@update_user' );
 
         // API VERSEMENTS
-        Route::get('versements/{Matri_Elev}', 'App\Http\Controllers\VersementController@versements');
-        Route::get('all_versements/{Matri_Elev}', 'App\Http\Controllers\VersementController@all_versements');
-        Route::get('last_versements/{Matri_Elev}', 'App\Http\Controllers\VersementController@last_versements');
+        Route::get( 'versements/{Matri_Elev}', 'App\Http\Controllers\VersementController@versements' );
+        Route::get( 'all_versements/{Matri_Elev}', 'App\Http\Controllers\VersementController@all_versements' );
+        Route::get( 'last_versements/{Matri_Elev}', 'App\Http\Controllers\VersementController@last_versements' );
 
         // API CERTIFICATS
-        Route::get('certificats/{Matri_Elev}', 'App\Http\Controllers\CertificatController@getCertificats');
-        Route::get('certificat_scol/{Matri_Elev}', 'App\Http\Controllers\CertificatController@getCertificats_scol');
+        Route::get( 'certificats/{Matri_Elev}', 'App\Http\Controllers\CertificatController@getCertificats' );
+        Route::get( 'certificat_scol/{Matri_Elev}', 'App\Http\Controllers\CertificatController@getCertificats_scol' );
 
         // API RELEVE DE NOTE
-        Route::get('releves/{Matri_Elev}', 'App\Http\Controllers\RelevesController@getannee');
+        Route::get( 'releves/{Matri_Elev}', 'App\Http\Controllers\RelevesController@getannee' );
+        Route::get( 'releves_semestre/{Matri_Elev}/{annee_acad}', 'App\Http\Controllers\RelevesController@getreleve' );
     }
 );
